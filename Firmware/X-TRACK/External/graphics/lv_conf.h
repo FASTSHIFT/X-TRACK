eed15781c6ed95c63f2a5d2eaf15bad3f2361982
_VIFEXTech <vifextech@foxmail.com>
2024-10-30 16:18:01 +0800

    fix(lv_conf): fix letter error

--- Firmware/X-TRACK/External/graphics/lv_conf.h
+++ Firmware/X-TRACK/External/graphics/lv_conf.h
@@ -788,7 +788,7 @@
 /** API for open, read, etc. */
 #define LV_USE_FS_POSIX 1
 #if LV_USE_FS_POSIX
-    #define LV_FS_POSIX_LETTER 'A'      /**< Set an upper cased letter on which the drive will accessible (e.g. 'A') */
+    #define LV_FS_POSIX_LETTER '/'      /**< Set an upper cased letter on which the drive will accessible (e.g. 'A') */
     #define LV_FS_POSIX_PATH ""         /**< Set the working directory. File/directory paths will be appended to it. */
     #define LV_FS_POSIX_CACHE_SIZE 0    /**< >0 to cache this number of bytes in lv_fs_read() */
 #endif
