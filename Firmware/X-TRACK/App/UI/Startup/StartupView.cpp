4354229ee691a7bd833c304d65fe2848180cbf28
_VIFEXTech <vifextech@foxmail.com>
2024-11-01 15:15:30 +0800

    feat(StartupView): using traditional animation

--- Firmware/X-TRACK/App/UI/Startup/StartupView.cpp
+++ Firmware/X-TRACK/App/UI/Startup/StartupView.cpp
@@ -1,6 +1,6 @@
 /*
  * MIT License
- * Copyright (c) 2023 _VIFEXTech
+ * Copyright (c) 2023 - 2024 _VIFEXTech
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -34,72 +34,39 @@ StartupView::StartupView(EventListener* listener, lv_obj_t* root)
     , _font(36, "medium")
 {
     static_assert(sizeof(StartupView) >= (size_t)MSG_ID::_LAST, "Large MSG_ID");
-    int32_t contWidth = 0;
-    int32_t contHeight = 0;
 
-    lv_obj_t* contBottom = lv_obj_create(root);
-    {
-        lv_obj_remove_style_all(contBottom);
-        lv_obj_clear_flag(contBottom, LV_OBJ_FLAG_SCROLLABLE);
+    lv_obj_t* cont = lv_obj_create(root);
+    lv_obj_remove_style_all(cont);
+    lv_obj_clear_flag(cont, LV_OBJ_FLAG_SCROLLABLE);
+    lv_obj_set_style_border_color(cont, COLOR_ORANGE, 0);
+    lv_obj_set_style_border_side(cont, LV_BORDER_SIDE_BOTTOM, 0);
+    lv_obj_set_style_border_width(cont, 3, 0);
+    lv_obj_set_style_border_post(cont, true, 0);
+    lv_obj_center(cont);
 
-        lv_obj_t* label = lv_label_create(contBottom);
-        lv_obj_set_style_text_font(label, _font, 0);
-        lv_label_set_text_static(label, VERSION_FIRMWARE_NAME);
-        lv_obj_update_layout(label);
+    lv_obj_t* label = lv_label_create(cont);
+    lv_obj_set_style_text_font(label, _font, 0);
+    lv_label_set_text_static(label, VERSION_FIRMWARE_NAME);
 
-        contWidth = lv_obj_get_width(label);
-        contHeight = lv_obj_get_height(label);
-        lv_obj_set_size(contBottom, contWidth, contHeight);
-        lv_obj_align_to(contBottom, root, LV_ALIGN_CENTER, 0, 0);
-    }
-
-    lv_obj_t* contTop = lv_obj_create(root);
-    {
-        lv_obj_remove_style_all(contTop);
-        lv_obj_set_style_bg_color(contTop, COLOR_ORANGE, 0);
-        lv_obj_set_style_bg_opa(contTop, LV_OPA_COVER, 0);
-        lv_obj_set_size(contTop, contWidth, contHeight);
-        lv_obj_clear_flag(contTop, LV_OBJ_FLAG_SCROLLABLE);
-        lv_obj_align_to(contTop, root, LV_ALIGN_CENTER, 0, 0);
-    }
-
-    lv_obj_t* labelTop = lv_label_create(contTop);
-    {
-        lv_obj_set_style_text_font(labelTop, _font, 0);
-        lv_obj_set_style_text_color(labelTop, lv_color_black(), 0);
-        lv_obj_set_align(labelTop, LV_ALIGN_LEFT_MID);
-        lv_label_set_text_static(labelTop, VERSION_FIRMWARE_NAME);
-    }
-
-    auto set_trans_x = [](void* obj, int32_t x) {
-        lv_obj_set_style_translate_x((lv_obj_t*)obj, x, 0);
-    };
-
-    /* Since lv_anim checks exec_cb, set_w here is split into two functions */
-    auto set_w = [](void* obj, int32_t w) {
-        lv_obj_set_width((lv_obj_t*)obj, w);
-    };
+    lv_obj_update_layout(label);
+    int32_t contWidth = lv_obj_get_width(label);
+    int32_t contHeight = lv_obj_get_height(label);
+    lv_obj_set_size(cont, contWidth, contHeight);
 
     _anim_timeline = lv_anim_timeline_create();
 
-    auto anim_path = lv_anim_path_ease_out;
-
-    /* LV_ANIM_EXEC is used to distinguish two function pointers
-     * and bypass lv_anim's duplication check on exec_cb.
-     */
+#define ANIM_DEF(start_time, obj, attr, start, end) \
+    { start_time, obj, LV_ANIM_EXEC(attr), start, end, 500, lv_anim_path_ease_out, true }
 
     lv_anim_timeline_wrapper_t wrapper[] = {
-        { 0, contTop, set_w, 0, contWidth, 500, anim_path, false },
-        { 500, contTop, LV_ANIM_EXEC(width), contWidth, 0, 500, anim_path, false },
-        { 500, contTop, set_trans_x, 0, contWidth, 500, anim_path, false },
-        { 500, labelTop, set_trans_x, 0, -contWidth, 500, anim_path, false },
-        { 500, contBottom, set_w, 0, contWidth, 500, anim_path, true },
+        ANIM_DEF(0, cont, width, 0, contWidth),
+        ANIM_DEF(500, label, y, contHeight, lv_obj_get_y(label)),
         LV_ANIM_TIMELINE_WRAPPER_END
     };
 
     lv_anim_timeline_add_wrapper(_anim_timeline, wrapper);
 
-    subscribe(MSG_ID::SHOW, contTop,
+    subscribe(MSG_ID::SHOW, cont,
         [](lv_event_t* e) {
             auto self = (StartupView*)lv_event_get_user_data(e);
             auto msg = lv_event_get_msg(e);
@@ -114,7 +81,7 @@ StartupView::StartupView(EventListener* listener, lv_obj_t* root)
             lv_anim_timeline_start(self->_anim_timeline);
         });
 
-    subscribe(MSG_ID::FADE_OUT, contBottom,
+    subscribe(MSG_ID::FADE_OUT, cont,
         [](lv_event_t* e) {
             auto self = (StartupView*)lv_event_get_user_data(e);
             auto obj = lv_event_get_current_target_obj(e);
